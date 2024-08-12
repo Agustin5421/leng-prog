f19c24a2bcd951064c5be21a3d87a7a0076f7588
Agustin <almartinez1@mail.austral.edu.ar>
2024-08-12 14:31:03 -0300

    implemented collatz tp

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz a = if a <= 0 then Nothing else Just (collatz' a 0)
+  where
+    collatz' a b
+      | a == 1 = b
+      | even a = collatz' (a `div` 2) (b + 1)
+      | odd a = collatz' (3 * a + 1) (b + 1)
